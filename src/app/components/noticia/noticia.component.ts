92b4f926f1341b4f9c816a5595e7af9328ebf42a
josecoborivas <josecoborivas@gmail.com>
2020-06-13 13:37:39 -0300

    noticias

---
+++ src/app/components/noticia/noticia.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from '../../intefaces/interfaces';
+
+@Component({
+  selector: 'app-noticia',
+  templateUrl: './noticia.component.html',
+  styleUrls: ['./noticia.component.scss'],
+})
+export class NoticiaComponent implements OnInit {
+
+  @Input() noticia: Article;
+  @Input() index: number;
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+}
